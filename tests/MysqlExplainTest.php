64d5f4436fc8682fa7fa4e3bc604f307535bb317
tpetry <github@tpetry.me>
2024-09-13 12:55:28 +0200

    fix: query exception had different parameters in old versions

--- tests/MysqlExplainTest.php
+++ tests/MysqlExplainTest.php
@@ -6,6 +6,7 @@ namespace Tpetry\MysqlExplain\Tests;
 
 use Illuminate\Database\ConnectionInterface;
 use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
 use Mockery\MockInterface;
 use RuntimeException;
@@ -45,7 +46,10 @@ class MysqlExplainTest extends TestCase
                 ->andReturn('mysql');
             $mock->shouldReceive('queryScalar')
                 ->withArgs([$connection, 'SELECT * FROM seq_1_to_1'])
-                ->andThrow(new QueryException("SQLSTATE[42S02]: Base table or view not found: 1146 Table 'seq_1_to_1' doesn't exist.", 'SELECT * FROM seq_1_to_1', [], new RuntimeException));
+                ->andThrow(match (true) {
+                    version_compare(App::version(), '10.0.0', '>=') => new QueryException('mysql', 'SELECT * FROM seq_1_to_1', [], new RuntimeException),
+                    default => new QueryException('SELECT * FROM seq_1_to_1', [], new RuntimeException)
+                });
             $mock->shouldReceive('queryScalar')
                 ->withArgs([$connection, 'SELECT VERSION()'])
                 ->andReturn('...version...');
